a74d402de9bb7157eaccde57ff9833d962359c80
curiosankitkumar <ankitdark719@gmail.com>
2024-02-05 21:45:44 +0530

    adden some question in c language

--- New.c
+++ New.c
@@ -1,6 +1,260 @@
-#include <stdio.h>
-int main(){
-    printf("Hello WOrld!");
-    printf("This is new!");
-    return 0;
-}
+// Write a C program to print all natural numbers from 1 to n using loop. C program to print 
+// first n natural numbers using loop. How to print natural numbers in a given range using 
+// loop. Logic to print natural numbers using for loop in C program.
+
+// Example Input
+
+// Input upper limit: 10
+
+// #include<stdio.h>
+// int main() {
+//     int i , n;
+//     printf("Enter any number : ");
+//     scanf("%d", &n);
+
+//     for (i=0; i<=10; i++)
+//     {
+//         printf("%d\n  ", i);
+//     }
+// }
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int i , n;
+//     printf(" Enter one natural number 1 to 10 : ");
+//     scanf("%d", &n);
+
+//     i = 0;
+//     while (i <= 10)
+//     {
+//         printf("%d\n", i );
+//         i++;
+//     }
+    
+// }
+
+//Write a C program to print all natural numbers in reverse from n to 1 using for loop. 
+// How to print natural numbers in reverse order in C programming. Logic to print natural 
+// numbers in reverse for a given range in C program.
+
+// Example Input
+
+// Input N: 10
+
+// #include<stdio.h>
+// int main () {
+//     int i , n ;
+//     printf("ENter any number: ");
+//     scanf("%d" , &n);
+
+//     i = 10;
+
+//     while (i>=1)
+//     {
+//         printf("%d\n",i);
+//         i--;
+//     }
+// }
+
+// #include<stdio.h>
+// int main (){
+//     int i, n;
+//     printf("Enter any number :");
+//     scanf("%d", &n);
+
+//     for (i=10; i>=1; i--){
+//         printf("%d\n", i);
+//     }
+// }
+
+// Write a C program to print all alphabets from a to z using while loop. How to display 
+// alphabets from a to z using while loop in C programming.
+
+// Example Input
+
+// Output
+
+// Alphabets: a, b, c, d, e, ... , z
+
+// #include<stdio.h>
+
+// int main (){
+//     char i, ch;
+//     printf("Enter any character: ");
+//     scanf("%c", &ch);
+
+//     for (i='a'; i <= 'z'; i++)
+//     {
+//         printf("%c\n", i);
+//     }
+// }
+
+// #include<stdio.h>
+// int main (){
+//     char i ,ch;
+//     printf("Enter any character a to z :");
+//     scanf("%d", &ch);
+
+//     i='a';
+
+//     while (i<='z')
+//     {
+//         printf("%c\n", i);
+//         i++;
+//     }
+//}
+
+//Q--Write a C program to print all even numbers from 1 to n using for loop. C program to generate 
+// all even numbers between given range. Logic to print even numbers using if else and for loop in 
+// given range in C programming.
+
+// Example Input
+
+// Input upper range: 10
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int i , n;
+//     printf("Enter any  number: ");
+//     scanf("%d",&n);
+
+//     printf("Even numbers from 1 to %d are: \n", n);
+
+//     for (i=2; i<=n; i++)
+//     {
+//         if (i % 2 == 0){
+//             printf("%d\n", i);
+//         }
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
