d6c5e247bc54bb0bfa7349753e844c8bba65eb28
curiosankitkumar <curios.ankitkumar@gmail.com>
2024-02-13 15:04:41 +0530

    logic

--- switch/Fun1.c
+++ switch/Fun1.c
@@ -183,19 +183,40 @@
 //     printHW(count-1);
 // }
 
+// #include<stdio.h>
+
+// int sum (int n);
+
+// int main (){
+//     printf("sum is : %d",sum(5));
+//     return 0;
+// }
+// int sum (int n){
+//     if (n == 1){
+//         return 1;
+//     }
+//     int sumNm1 = sum(n-1); // sum of 1 to n
+//     int sumN = sumNm1 + n;
+//     return sumN;
+// }
+
+
+// Q -- Factorial of n.
+
 #include<stdio.h>
 
-int sum (int n);
+int fact (int n);
 
 int main (){
-    printf("sum is : %d",sum(5));
-    return 0;
+    printf("factorail is : %d", fact(4));
+    return 0 ;
 }
-int sum (int n){
-    if (n == 1){
+
+int fact (int n ) {
+    if (n==0){
         return 1;
     }
-    int sumNm1 = sum(n-1); // sum of 1 to n
-    int sumN = sumNm1 + n;
-    return sumN;
+    int factNm1 = fact(n-1);
+    int factN = factNm1 * n ;
+    return factN;
 }
